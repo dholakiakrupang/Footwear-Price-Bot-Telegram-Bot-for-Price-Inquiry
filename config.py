db086af79ca28a4f549767dbe7ac970c00bc605d
dholakiakrupang <162746532+dholakiakrupang@users.noreply.github.com>
2024-06-14 11:49:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ API_TOKEN = os.getenv("TELEGRAM_API_TOKEN")
 if not API_TOKEN:
     raise ValueError("No TELEGRAM_API_TOKEN found in environment variables")
 
-APPROVED_USERS = ["dholakiakrupang","Dhruvilathigara","dhirendradholakia"]  # List of approved users
+APPROVED_USERS = ["dholakiakrupang"]  # List of approved users
 SPECIAL_USERS = ["dholakiakrupang"]  # List of special users
 ADMIN_USERS = ["admin_user"]  # List of admin users
